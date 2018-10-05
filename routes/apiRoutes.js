00b30ff7e1dcdc7e8f10f9fdf21249f3531620f4
JLasc <lascaze.j@gmail.com>
2018-10-05 18:21:22 -0400

    pages linked together

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,49 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  /*   app.get("/api/examples", function (req, res) {
+      db.Example.findAll({}).then(function (dbExamples) {
+        res.json(dbExamples);
+      });
     });
+
+    // Create a new example
+    app.post("/api/examples", function (req, res) {
+      db.Example.create(req.body).then(function (dbExample) {
+        res.json(dbExample);
+      });
+    });
+
+    // Delete an example by id
+    app.delete("/api/examples/:id", function (req, res) {
+      db.Example.destroy({
+        where: {
+          id: req.params.id
+        }
+      }).then(function (dbExample) {
+        res.json(dbExample);
+      });
+    }); */
+
+
+  //Get All Question in API\\
+  app.get("/api/question", function (req, res) {
+    res.json(res)
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
 
-   // Question View \\ 
-   app.get('/question', function(req, res){
+  // Add Question to API \\
+  app.post("/api/question", function (req, res) {
+
+  })
+
+
+  // Question View \\ 
+  app.get('/question', function (req, res) {
     var answerArr = [];
-    db.Question.findAll({}).then(function(question){
+    db.Question.findAll({}).then(function (question) {
       /* res.render("question") */
       res.render("question", {
         question: question[0].text,
@@ -37,12 +55,13 @@ module.exports = function(app) {
         where: {
           QuestionId: 2
         }
-      }).then(function(answer){
-        for (i=0; i<answer.length; i++){
+      }).then(function (answer) {
+        for (i = 0; i < answer.length; i++) {
           answerArr.push(answer[i].response)
         }
         console.log(answerArr.length)
       })
     })
   });
+
 };
