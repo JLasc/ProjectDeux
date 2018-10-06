cab28300ddd23717ed7878a10dd6c5d4c176f3cd
JLasc <lascaze.j@gmail.com>
2018-10-06 11:22:21 -0400

    updates

--- server.js
+++ server.js
@@ -56,39 +56,3 @@ db.sequelize.sync(syncOptions).then(function() {
 });
 
 module.exports = app;
-
-users = [];
-connections = [];
-
-io.sockets.on('connection', function(socket) {
-  connections.push(socket);
-  console.log('Connected: %s sockets connected', connections.length);
-
-  // Disconnect
-  socket.on('disconnect', function(data) {
-      
-      users.splice(users.indexOf(socket.username), 1);
-      updateUsernames();
-      connections.splice(connections.indexOf(socket), 1);
-      console.log('Disconnected: %s sockets connected', connections.length);
-  });
-
-  // send message
-  socket.on('send message', function(data) {
-      console.log(data);
-      io.sockets.emit('new message', {msg:data, user: socket.username});
-  })
-
-  // new user
-  socket.on('new user', function(data, callback) {
-      callback(true);
-      socket.username = data;
-      users.push(socket.username);
-      updateUsernames();
-  })
-
-  function updateUsernames() {
-      io.sockets.emit('get users', users);
-  }
-  
-})
